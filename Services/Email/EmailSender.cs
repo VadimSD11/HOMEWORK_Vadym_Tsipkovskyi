e7b7e70535b252d7d79be8cc346044f1defa252d
VadimSD11 <71723814+VadimSD11@users.noreply.github.com>
2023-05-02 17:47:43 +0300

    Add files via upload

---
+++ Services/Email/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace WebApplication5.Services.Email
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //TODO Create Email service
+            await Task.CompletedTask;
+        }
+    }
+}
