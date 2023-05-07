b45869435cee6eccb8ba843f9a1cc70a667b1262
VadimSD11 <71723814+VadimSD11@users.noreply.github.com>
2023-05-07 15:47:49 +0300

    Delete Data directory

--- Data/Identity/ApplicationIdentityUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-namespace WebApplication5.Data.Identity
-{
-    public class ApplicationIdentityUser:IdentityUser
-    {
-        public long ApplicationId { get; set; }
-        public string Name { get; set; }
-    }
-}
